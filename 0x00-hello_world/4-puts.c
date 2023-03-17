b9209b7c7de976d3b2575a27f540ecd24918d2e1
pmutizira24 <pmutizira24@gmail.com>
2023-03-17 15:31:38 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	4. Hello, puts
+mandatory
+Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+Use the function puts
+You are not allowed to use printf
+Your program should end with the value 0
+vi 4-puts.c
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
+
+
